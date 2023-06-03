9169e361da64e903043d65cf1e0c5885a4b9d571
ferbragon <nando.gon17@gmail.com>
2023-06-03 11:12:41 -0600

    styles(app/users/[id]/page.jsx): Add bootstrap styles to UsersPage

--- nextjs-firstproject/app/users/[id]/page.jsx
+++ nextjs-firstproject/app/users/[id]/page.jsx
@@ -9,12 +9,16 @@ async function UsersPage({ params }){
 
     const user = await getUser(params.id);
     return(
-        <div>
-            <h1>User Details</h1>
-            <div>
-                <img src={user.avatar} alt={user.first_name} />
-                <h3>{user.id} {user.first_name} {user.last_name}</h3>
-                <p>{user.email}</p>
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <div className="card">
+                    <div className="card-header text-center">
+                        <img src={user.avatar} alt={user.first_name} style={{ borderRadius: "50%"}}/>                </div>
+                    <div className="card-body text-center">
+                        <h3>{user.id} {user.first_name} {user.last_name}</h3>
+                        <p>{user.email}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
