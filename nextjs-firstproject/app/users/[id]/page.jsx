04bd2ded23b0b7919ba7ef984a660ed93097d0ca
ferbragon <nando.gon17@gmail.com>
2023-06-03 10:03:15 -0600

    feat(/app/users/[id]): Create getUser Server component

--- nextjs-firstproject/app/users/[id]/page.jsx
+++ nextjs-firstproject/app/users/[id]/page.jsx
@@ -1,8 +1,21 @@
+async function getUser(id){
+    const res = await fetch(`https://reqres.in/api/users/${id}`);
+    const data = await res.json();
+    return data.data;
+}
 
-function UsersPage(){
+
+async function UsersPage({ params }){
+
+    const user = await getUser(params.id);
     return(
         <div>
             <h1>User Details</h1>
+            <div>
+                <img src={user.avatar} alt={user.first_name} />
+                <h3>{user.id} {user.first_name} {user.last_name}</h3>
+                <p>{user.email}</p>
+            </div>
         </div>
     )
 };
