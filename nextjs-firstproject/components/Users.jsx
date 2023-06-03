e93d85cdfa055c6ab7d84dd52eca19da92d6c516
ferbragon <nando.gon17@gmail.com>
2023-06-03 12:00:38 -0600

    style(components/Users.js): Add style bootstrap to Users component

--- nextjs-firstproject/components/Users.jsx
+++ nextjs-firstproject/components/Users.jsx
@@ -6,23 +6,22 @@ function Users({ users }) {
     const router = useRouter();
 
     return(
-        <div>
-            <ul>
+            <ul className="list-group">
             {
                 users.map((user, index) => (
                 <li key={index}
+                    className="list-group-item d-flex justify-content-between align-items-center list-group-item-action"
                     onClick={() => router.push(`/users/${user.id}`)}
                 >
                     <div>
                     <h5>{user.id} {user.first_name} {user.last_name}</h5>
                     <p>{user.email}</p>
                     </div>
-                    <img src={user.avatar} alt={user.email} />
+                    <img src={user.avatar} alt={user.email} style={{ borderRadius: "50%" }} />
                 </li>
                 ))
             }
             </ul>
-        </div>
     )
 };
 
